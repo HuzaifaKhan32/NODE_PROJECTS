d7d48c6a8207d20de21398642843cbb627719359
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-20 01:41:01 +0500

    Completed Project

---
+++ Question6/app.js
@@ -0,0 +1,4 @@
+var NameWithWhitespaces = "\t Kamran Tessori \n";
+console.log("Name with white spaces:".concat(NameWithWhitespaces));
+var NameWithoutWhitespaces = NameWithWhitespaces.trim();
+console.log("Name without white spaces:".concat(NameWithoutWhitespaces));
--- Question6/app.ts
+++ Question6/app.ts
@@ -1,2 +1,6 @@
-let Name = "\t Kamran Tessori \n";
-console.log(`Name with white spaces${Name}`);
+let NameWithWhitespaces = "\t Kamran Tessori \n";
+console.log(`Name with white spaces:${NameWithWhitespaces}`);
+let NameWithoutWhitespaces = NameWithWhitespaces.trim();
+console.log(`Name without white spaces:${NameWithoutWhitespaces}`);
+
+
