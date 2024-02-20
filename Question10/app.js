c4bf87fa13c2fe75411e4c3646e4d05775b11af0
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-21 00:41:25 +0500

    New commit

---
+++ Question10/app.js
@@ -0,0 +1,12 @@
+// 1st Program
+// This Program prints the Quote with the name of the author
+var Quote1 = "'It is never too late to be what you might have been. '";
+var author1 = "George Eliot once said,";
+// This line of code plays the role of printing the Quote and the person's name who said this quote on the console. 
+console.log(author1, Quote1);
+// 2nd Program
+// This Program prints the name in lower case means in small alphabets
+var Name_ = "Huzaifa";
+// result_ variable convert the string into small alphabets  
+var result_ = Name_.toLowerCase();
+console.log(result_);
---
+++ Question10/app.ts
@@ -0,0 +1,13 @@
+// 1st Program
+// This Program prints the Quote with the name of the author
+let Quote1 = "'It is never too late to be what you might have been. '";
+let author1 = "George Eliot once said,";
+// This line of code plays the role of printing the Quote and the person's name who said this quote on the console. 
+console.log(author1, Quote1);
+
+// 2nd Program
+// This Program prints the name in lower case means in small alphabets
+let Name_ = "HUZAIFA";
+// result_ variable convert the string into small alphabets  
+let result_ = Name_.toLowerCase();
+console.log(result_);
