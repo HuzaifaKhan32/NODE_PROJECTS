ffb393e9966979640f3762486d7cb846c172a8df
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-21 00:43:31 +0500

    New commit

---
+++ Question9/app.js
@@ -0,0 +1,2 @@
+var favorite_num = 56;
+console.log("My favorite number is ".concat(favorite_num));
---
+++ Question9/app.ts
@@ -0,0 +1,2 @@
+let favorite_num : number = 56;
+console.log(`My favorite number is ${favorite_num}`) ;
