be69b8376faf1b8ea97808ce5e1f9f325b912b2e
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-21 23:56:16 +0500

    Add a new file

--- Question11/app.ts
+++ Question11/app.ts
@@ -1,5 +1,4 @@
 let friend_names : string[] = ["Ebtisam", "Ghilman", "Abdul Wasey", "Hashir"];
-console.log(friend_names[0]);
-console.log(friend_names[1]);
-console.log(friend_names[2]);
-console.log(friend_names[3]);
+for(let friend of friend_names){
+    console.log(friend);
+}
