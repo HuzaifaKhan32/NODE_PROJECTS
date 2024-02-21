bca4b0942deffc1c675626c5f6e814c7a9593654
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-21 23:14:48 +0500

    New commit

---
+++ Question11/app.js
@@ -0,0 +1,12 @@
+var friend_names = ["Ebtisam", "Ghilman", "Abdul Wasey", "Hashir"];
+// console.log(friend_names[0]);
+// console.log(friend_names[1]);
+// console.log(friend_names[2]);
+// console.log(friend_names[3]);
+for (var _i = 0, friend_names_1 = friend_names; _i < friend_names_1.length; _i++) {
+    var name_1 = friend_names_1[_i];
+    console.log(name_1);
+}
+for (var i = 0; i < friend_names.length; i++) {
+    console.log(friend_names[i]);
+}
---
+++ Question11/app.ts
@@ -0,0 +1,5 @@
+let friend_names : string[] = ["Ebtisam", "Ghilman", "Abdul Wasey", "Hashir"];
+console.log(friend_names[0]);
+console.log(friend_names[1]);
+console.log(friend_names[2]);
+console.log(friend_names[3]);
