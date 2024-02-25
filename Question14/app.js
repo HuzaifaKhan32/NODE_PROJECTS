21eebb49187ac90a62d436235273d7d98a04d5a3
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-25 23:14:05 +0500

    Adding some modules

---
+++ Question14/app.js
@@ -0,0 +1,2 @@
+var guest_names = ["Mr. Kalam", "Mr. Abdul Rauf", "Mr. Iqbal", "Mr. Mohsin"];
+console.log("".concat(guest_names[2], " Plz have a dinner with us on saturday night"));
---
+++ Question14/app.ts
@@ -0,0 +1,2 @@
+let guest_names : string [] = ["Mr. Kalam", "Mr. Abdul Rauf", "Mr. Iqbal", "Mr. Mohsin"];
+console.log(`${guest_names[2]} Plz have a dinner with us on saturday night`);
