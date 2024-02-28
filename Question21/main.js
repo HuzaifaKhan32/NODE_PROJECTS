e46ea2ff26e8b221e89a6292052a0976e37a6850
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-29 02:07:47 +0500

    New file

---
+++ Question21/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let think_object = {
+    country: "Egypt",
+    city: "Giza",
+    place: "The Great Pyramid of Giza",
+    days_of_visit: 25
+};
+console.log(think_object);
---
+++ Question21/main.ts
@@ -0,0 +1,9 @@
+
+let think_object : {country : string, city : string, place : string, days_of_visit : number } = {
+    country : "Egypt",
+    city : "Giza",
+    place : "The Great Pyramid of Giza",
+    days_of_visit : 25 
+}
+
+console.log(think_object);
