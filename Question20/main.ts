0c83cb5598c7d6926d2be1179676191307b54566
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-29 01:47:50 +0500

    New File

---
+++ Question20/main.ts
@@ -0,0 +1,5 @@
+let languages : string[] = ["Urdu", "English", "French", "Spanish", "Hindi", "Arabic", "Chineese"];
+console.log("List of Languages");
+for(let List of languages ){
+    console.log(List);
+}
