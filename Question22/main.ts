1d78599f43a703d5480c178fbd6067d1591f47e5
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-02-29 23:47:40 +0500

    New File

---
+++ Question22/main.ts
@@ -0,0 +1,12 @@
+import { error } from "console";
+
+let arr : number[] = [1,4,56,75,23,45,2];
+console.log(arr[7]);
+console.log(arr[-2]);
+
+
+
+// Lets correct the error
+console.log(arr[6]);
+console.log(arr[2]);
+
