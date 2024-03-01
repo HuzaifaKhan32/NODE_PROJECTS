fa3715b794aa10cffbb90a66ffb540ecbe547822
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-02 01:22:31 +0500

    New File

---
+++ Question29/main.js
@@ -0,0 +1,10 @@
+var favorite_fruits = ["Banana", "Mango", "Apple"];
+if (favorite_fruits.includes("Banana")) {
+    console.log("You really like Bananas!");
+}
+if (favorite_fruits.includes("Mango")) {
+    console.log("You really like Mango!");
+}
+if (favorite_fruits.includes("Apple")) {
+    console.log("You really like Apple!");
+}
