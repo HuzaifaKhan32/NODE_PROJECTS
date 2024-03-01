d189f881a8323a68b9486b52888aae0a566f22ae
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-01 22:12:57 +0500

    Adding

--- Question24/main.js
+++ Question24/main.js
@@ -5,11 +5,11 @@ let test_string2 = "Hello";
 // Tests for equality and inequality with strings
 console.log("Equialty test:", test_string == test_string2); // True
 console.log("Inequialty test:", test_string !== test_string2); // False
-console.log("Equialty test with strings:", test_string === test_string2);
-console.log("Inequialty test with strings:", test_string !== test_string2);
+console.log("Equialty test with strings:", test_string === test_string2); // True
+console.log("Inequialty test with strings:", test_string !== test_string2); // False
 // Tests using the lower case function
-console.log("Equality Test with lowercase function:", test_string.toLowerCase() === "hello");
-console.log("Inequality Test with lowercase function:", test_string.toLowerCase() !== "hello");
+console.log("Equality Test with lowercase function:", test_string.toLowerCase() === "hello"); // True
+console.log("Inequality Test with lowercase function:", test_string.toLowerCase() !== "hello"); // False
 // Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
 let num = 5;
 let num1 = 7;
@@ -20,5 +20,10 @@ console.log("Less Than Test:", num < num1); // True
 console.log("Greater Than or Equal To Test:", num >= num1); // False
 console.log("Less Than or Equal To Test:", num <= num1); // True
 // Tests using "and" and "or" operators
-console.log("Testing with AND", num === num1 && num !== num1);
-console.log("Testing with OR", num === num1 || num !== num1);
+console.log("Testing with AND", num === num1 && num !== num1); // False
+console.log("Testing with OR", num === num1 || num !== num1); // True
+// Test whether an item is in a array
+let arr = ["Huzaifa", "Ghilman", "Ebtisam", "Abdul Wasey"];
+console.log("Equality test with array", arr.includes("Huzaifa")); //True
+// Test whether an item is not in a array
+console.log("Inequality test with array", arr.includes("Aseem")); //False
