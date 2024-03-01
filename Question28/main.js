cd67e0849c3886910616ad3b5a8f68bff6dacbb8
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-02 01:07:39 +0500

    New File

---
+++ Question28/main.js
@@ -0,0 +1,19 @@
+var age = 12;
+if (age < 2) {
+    console.log("The person is a baby");
+}
+else if (age < 4) {
+    console.log("The person is toddler");
+}
+else if (age < 13) {
+    console.log("The person is Kid");
+}
+else if (age < 20) {
+    console.log("The person is Teenager");
+}
+else if (age < 65) {
+    console.log("The person is Adult");
+}
+else {
+    console.log("You are elder");
+}
