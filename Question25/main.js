90de0b69da133ec7726d7fd4a40a0a1a94803baf
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-01 22:27:16 +0500

    New file

---
+++ Question25/main.js
@@ -0,0 +1,9 @@
+var allien_color = "Green";
+// Passed Program 
+if (allien_color == "Green") {
+    console.log("You just earned 5 points!");
+}
+// Failed Program 
+if (allien_color == "yellow") {
+    console.log("Failed Program");
+}
