5e9383b5de06f4ba208d219549eabda292b790da
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-02 00:33:59 +0500

    New File

---
+++ Question26/main.js
@@ -0,0 +1,15 @@
+var AllienColor = "Green";
+// In this if-else statement if block will execute  
+if (AllienColor == "Green") {
+    console.log("You just have earned 5 points!");
+}
+else {
+    console.log("You earned 10 points!");
+}
+// In this if-else statement else block will execute
+if (AllienColor == "Yellow") {
+    console.log("You just have earned 5 points!");
+}
+else {
+    console.log("You earned 10 points!");
+}
