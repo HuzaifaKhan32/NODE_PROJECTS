da36b166d3e742e59508d20d2f6cbc2ab8ce5546
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-02 00:50:45 +0500

    New File

---
+++ Question27/main.js
@@ -0,0 +1,10 @@
+var allienColor = "Yellow";
+if (allienColor == "Green") {
+    console.log("You earned 5 points!");
+}
+else if (allienColor == "Yellow") {
+    console.log("You earned 10 points!");
+}
+else {
+    console.log("You earned 15 points!");
+}
