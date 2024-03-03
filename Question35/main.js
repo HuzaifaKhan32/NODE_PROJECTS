c38f967a79e3e96886a306cac999971d674be576
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-04 01:00:05 +0500

    .

---
+++ Question35/main.js
@@ -0,0 +1,6 @@
+var AnimalNames = ["Dog", "Cat", "Goat"];
+for (var _i = 0, AnimalNames_1 = AnimalNames; _i < AnimalNames_1.length; _i++) {
+    var names = AnimalNames_1[_i];
+    console.log("".concat(names, " would make a great pet."));
+}
+console.log("Any of these animals would make a great pet!");
