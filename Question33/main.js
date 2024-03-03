94c2e0a3d7e5e58db00000d2ccca22e45f179a6e
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-04 00:45:26 +0500

    .

--- Question33/main.js
+++ Question33/main.js
@@ -1,14 +1,18 @@
-var OridinalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for (var _i = 0, OridinalNumbers_1 = OridinalNumbers; _i < OridinalNumbers_1.length; _i++) {
-    var numbers = OridinalNumbers_1[_i];
+/*Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a array.
+• Loop through the array.
+• Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line. */
+var OrdinalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (var _i = 0, OrdinalNumbers_1 = OrdinalNumbers; _i < OrdinalNumbers_1.length; _i++) {
+    var numbers = OrdinalNumbers_1[_i];
     if (numbers == 1) {
-        console.log("1st");
+        console.log("".concat(numbers, "st"));
     }
     else if (numbers == 2) {
-        console.log("2nd");
+        console.log("".concat(numbers, "nd"));
     }
     else if (numbers == 3) {
-        console.log("3rd");
+        console.log("".concat(numbers, "rd"));
     }
     else {
         console.log(numbers + "th");
--- Question33/main.ts
+++ Question33/main.ts
@@ -5,13 +5,13 @@
 let OrdinalNumbers : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 for(let numbers of OrdinalNumbers){
     if(numbers == 1){
-        console.log("1st");
+        console.log(`${numbers}st`);
     }
     else if(numbers == 2){
-        console.log("2nd");
+        console.log(`${numbers}nd`);
     }        
     else if(numbers == 3){
-        console.log( "3rd");
+        console.log( `${numbers}rd`);
     }
     else{
         console.log(numbers + "th");
