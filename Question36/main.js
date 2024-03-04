13a53fcee8d82f29cdbe103e68c980fb867aa78f
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-05 00:32:23 +0500

    .

---
+++ Question36/main.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("You ordered a ".concat(size, " size shirt which has text \"").concat(message, "\""));
+}
+make_shirt("Medium", "Eat, Sleep, Code");
