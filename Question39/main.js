c7b34b4d15e0f221e3e154bd5e20119df30396ec
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-05 23:58:22 +0500

    .

---
+++ Question39/main.js
@@ -0,0 +1,4 @@
+function country_city(city, country) {
+    console.log("".concat(city, ", ").concat(country));
+}
+country_city("Lahore", "Pakistan");
---
+++ Question39/main.ts
@@ -0,0 +1,6 @@
+/*City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+"Lahore, Pakistan"*/
+function country_city(city : string, country : string){
+    console.log(`${city}, ${country}`);
+}
+country_city("Lahore", "Pakistan");
