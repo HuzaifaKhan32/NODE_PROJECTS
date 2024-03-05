5f87a7321d239d6c63d3bc903551adf13f6d6536
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-06 00:39:34 +0500

    .

---
+++ Question41/main.js
@@ -0,0 +1,8 @@
+var Magiciansnames = ["Merlin", "Houdini", "David Copperfield", "Harry Potter"];
+function magicions_name() {
+    for (var _i = 0, Magiciansnames_1 = Magiciansnames; _i < Magiciansnames_1.length; _i++) {
+        var name_1 = Magiciansnames_1[_i];
+        console.log(name_1);
+    }
+}
+magicions_name();
---
+++ Question41/main.ts
@@ -0,0 +1,8 @@
+/*Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.*/
+let Magiciansnames : string[] = ["Merlin", "Houdini", "David Copperfield", "Harry Potter"];
+function magicions_name(){
+    for(let name of Magiciansnames){
+        console.log(name);
+    }
+}
+magicions_name();
