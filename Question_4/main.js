3c74e772f1dc30bb15a399e2f0cc657334e4c414
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-11 01:30:39 +0500

    New File

---
+++ Question_4/main.js
@@ -0,0 +1,4 @@
+/* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:*/
+var Quote = "'It is never too late to be what you might have been. '";
+var author = "George Eliot once said,";
+console.log("".concat(author, " ").concat(Quote));
---
+++ Question_4/main.ts
@@ -0,0 +1,5 @@
+/* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:*/
+let Quote : string = "'It is never too late to be what you might have been. '";
+let author : string = "George Eliot once said,";
+// Using Template Literals
+console.log(`${author} ${Quote}`);
