efe05d5ffc388d66e135681b03cc9bdf1b82ff13
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-11 01:57:08 +0500

    New File

---
+++ Question_10/main.ts
@@ -0,0 +1,14 @@
+// 1st Program
+// This Program prints the Quote with the name of the author
+let quote = "'It is never too late to be what you might have been. '";
+let Author = "George Eliot once said,";
+// This line of code plays the role of printing the Quote and the person's name who said this quote, on the console. 
+console.log(Author, quote);
+
+
+
+// 1st Program
+// first I made a variable name : "name"
+let person_name = "Huzaifa"
+// Then I print a sentence in the terminal by using console.log function
+console.log(`Hello ${person_name}, would you like to learn some Python today?`);
