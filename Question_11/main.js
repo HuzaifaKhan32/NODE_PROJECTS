3a62eab602f54d5475188e06c3f87fc24666b8c9
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-11 02:01:25 +0500

    New File

---
+++ Question_11/main.js
@@ -0,0 +1,6 @@
+/*Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time. */
+var friend_names = ["Ebtisam", "Ghilman", "Abdul Wasey", "Hashir"];
+console.log(friend_names[0]);
+console.log(friend_names[1]);
+console.log(friend_names[2]);
+console.log(friend_names[3]);
