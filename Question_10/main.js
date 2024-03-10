39ec021d546223698c91d2b2d931d4e8abc2d788
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-11 01:58:01 +0500

    New File

---
+++ Question_10/main.js
@@ -0,0 +1,11 @@
+// 1st Program
+// This Program prints the Quote with the name of the author
+var quote = "'It is never too late to be what you might have been. '";
+var Author = "George Eliot once said,";
+// This line of code plays the role of printing the Quote and the person's name who said this quote, on the console. 
+console.log(Author, quote);
+// 1st Program
+// first I made a variable name : "name"
+var person_name = "Huzaifa";
+// Then I print a sentence in the terminal by using console.log function
+console.log("Hello ".concat(person_name, ", would you like to learn some Python today?"));
