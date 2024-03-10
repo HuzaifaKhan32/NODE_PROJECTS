a6038ee011a0fc9430dda1a252ea51fb88c29fcb
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-11 01:26:05 +0500

    New File

--- Question_3/main.js
+++ Question_3/main.js
@@ -1,6 +1,11 @@
 // Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var PersonName = "Huzaifa Khan";
+var PersonName = "huzaifa khan";
+// convert name into lower case
 console.log("Lower Case:", PersonName.toLowerCase());
-console.log("Lower Case:", PersonName.toUpperCase());
-var ToTitlecase = PersonName.charAt(0).toUpperCase() + PersonName.substring(1).toLowerCase();
-console.log("Lower Case:", ToTitlecase);
+// convert name into Upper case
+console.log("Upper Case:", PersonName.toUpperCase());
+// convert name into Title case
+var words = PersonName.split(" ");
+var ToTitlecase = words.map(function (word) { return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase(); });
+var result = ToTitlecase.join(" ");
+console.log("Title Case:", result);
--- Question_3/main.ts
+++ Question_3/main.ts
@@ -1,7 +1,14 @@
 // Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let PersonName : string = "Huzaifa Khan";
+let PersonName : string = "huzaifa khan";
+// convert name into lower case
 console.log("Lower Case:", PersonName.toLowerCase());
-console.log("Lower Case:", PersonName.toUpperCase());
-
-let ToTitlecase = PersonName.charAt(0).toUpperCase() + PersonName.substring(1).toLowerCase();
-console.log("Lower Case:", ToTitlecase);
+// convert name into Upper case
+console.log("Upper Case:", PersonName.toUpperCase());
+// convert name into Title case
+// using 'split' function to split a string into an array
+let words = PersonName.split(" ");
+// using 'map' function to make some changes with Elements of an array and return a new array  
+let ToTitlecase = words.map(word => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase());
+// using 'join' function to combine array into a string
+let result = ToTitlecase.join(" ");
+console.log("Title Case:", result);
