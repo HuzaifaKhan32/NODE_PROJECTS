65958bed5de30ba20ecbbf8223db51d79ca9d694
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 00:19:25 +0500

    New File

---
+++ Question_21/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let languages = ["Urdu", "English", "French", "Spanish", "Hindi", "Arabic", "Chineese"];
+console.log("List of Languages");
+for (let List of languages) {
+    console.log(List);
+}
---
+++ Question_21/main.ts
@@ -0,0 +1,9 @@
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.*/
+let think_object : {country : string, city : string, place : string, days_of_visit : number } = {
+    country : "Egypt",
+    city : "Giza",
+    place : "The Great Pyramid of Giza",
+    days_of_visit : 5 
+}
+
+console.log(think_object);
