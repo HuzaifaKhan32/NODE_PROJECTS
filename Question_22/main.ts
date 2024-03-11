bb6a7db301ea7d8c5a64d51764f23059468992c6
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 00:29:39 +0500

    New File

---
+++ Question_22/main.ts
@@ -0,0 +1,13 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let array : number[] = [1,4,56,75,23,45,2];
+// these are incorrect indices of array which results will be undefined
+console.log(array[7]);
+console.log(array[-2]);
+
+
+
+// Lets correct the error
+console.log(array[6]);
+console.log(array[2]);
+
+
