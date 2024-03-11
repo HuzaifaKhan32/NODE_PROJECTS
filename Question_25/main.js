a426e579f9eaa77e9b8af176d3952d69986a23f3
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 00:43:54 +0500

    New File

---
+++ Question_25/main.js
@@ -0,0 +1,13 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+var allien_color = "Green";
+// Passed Program 
+if (allien_color == "Green") {
+    console.log("You just earned 5 points!");
+}
+// Failed Program 
+var allien_color1 = "Yellow";
+if (allien_color1 == "Yellow") {
+    console.log("Failed Program");
+}
