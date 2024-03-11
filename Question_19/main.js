d5935e3795726517f4267187142c0dc46f418720
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 00:12:24 +0500

    New File

---
+++ Question_19/main.js
@@ -0,0 +1,6 @@
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+ */
+var reamaningGuest = ["Mr. Ghilman", "Mr.Kamal"];
+var Length = reamaningGuest.length;
+console.log("Number of guest Inviting: ".concat(Length));
---
+++ Question_19/main.ts
@@ -0,0 +1,6 @@
+/*Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+ */
+let reamaningGuest : string[] = ["Mr. Ghilman", "Mr.Kamal"];
+let Length = reamaningGuest.length
+console.log(`Number of guest Inviting: ${Length}`);
