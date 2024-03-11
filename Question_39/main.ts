b9b311b3b909bd741af8f75585f78fb59338b5e2
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 01:21:43 +0500

    New File

---
+++ Question_39/main.ts
@@ -0,0 +1,8 @@
+/*City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+"Lahore, Pakistan"*/
+function country_city(city : string, country : string) : void{
+    console.log(`${city}, ${country}`);
+}
+country_city("Lahore", "Pakistan");
+country_city("Berlin", "Germany");
+country_city("Makkah", "Saudi Arabia");
