68f55ba55cce5511c630e30599df237aa80c84e1
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-12 00:15:42 +0500

    New File

---
+++ Question_20/main.js
@@ -0,0 +1,7 @@
+/*Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items. */
+var languages = ["Urdu", "English", "French", "Spanish", "Hindi", "Arabic", "Chineese"];
+console.log("List of Languages");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var List = languages_1[_i];
+    console.log(List);
+}
---
+++ Question_20/main.ts
@@ -0,0 +1,6 @@
+/*Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items. */
+let languages : string[] = ["Urdu", "English", "French", "Spanish", "Hindi", "Arabic", "Chineese"];
+console.log("List of Languages");
+for(let List of languages ){
+    console.log(List);
+}
