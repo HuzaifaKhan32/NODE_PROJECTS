1c05f9cb585fb76faa98e29477e8fa8e8c8e1663
HuzaifaKhan32 <huzaifakhan0856551@gmail.com>
2024-03-23 00:15:38 +0500

    Removing some stuff

--- Question_18/main.ts
+++ Question_18/main.ts
@@ -1,4 +1,3 @@
-import { reverse } from "dns";
 
 /*Seeing the World: Think of at least five places in the world you’d like to visit.
 • Store the locations in a array. Make sure the array is not in alphabetical order.
